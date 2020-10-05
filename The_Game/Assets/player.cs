da2795f98a172ea7191fd370e518a15a0c5e760e
tplatt <theop@nmsu.edu>
2020-10-05 13:33:51 -0600

    added broken char movement stuff

---
+++ The_Game/Assets/player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    float horizontalMove = 0f;
+
+    public float runSpeed=40f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        //move character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+
+    }
+}
