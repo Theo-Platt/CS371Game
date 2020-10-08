38a27e785e2fbf8731e9d4f4809fa9d58a1888a6
Devin <devintha@nmsu.edu>
2020-10-07 20:46:48 -0600

    Added fire, AGAIN

---
+++ The_Game/Assets/Characters/BaseFIre.cs
@@ -0,0 +1,24 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// namespace Characters
+// {
+// //inherites base character class
+//     public class BaseFire : BaseCharacter
+//     {
+//         public BaseFire()
+//         {
+//             CharacterElementType = "Fire";
+//             CharacterDescription = "it's a lit";
+//             level = 1;
+//             hp = 10;
+//             attack = 2;
+//             defense = 4;
+//             speed = 5;
+//             accuracy = 5;
+//         } //end of Fire
+
+//     } //end of BaseFire
+
+// }
