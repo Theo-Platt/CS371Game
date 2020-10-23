237ba6c7b18cb3b530601a1cf34d216c3d3a7393
Devin <devintha@nmsu.edu>
2020-10-22 23:14:13 -0600

    Created BattleSystem that will hold game states

---
+++ The_Game/Assets/BattleSystem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BattleState {START, MIDDLE, PLAYERTURN, ENEMYTURN, WIN, LOSS}
+public class GameSys : MonoBehavior
+{
+    public GameObject player;
+    public GameObject enemy;
+
+    public Transform playerLocation;
+    public Transform enemyLocation;
+
+    public BattleState state;
+    
+    // Start called before the first frame update
+    void Start()
+    {
+        state = BattleState.START;
+        SetupBattle();
+    }
+
+    void SetupBattle()
+    {
+        Instantiate(player, playerLocation);
+        Instantiate(enemy, enemyLocation);
+            
+    }
+}
