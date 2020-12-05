7e9b700cc44f47a2231b08c2d196ecdf19f4575b
tplatt <theop@nmsu.edu>
2020-12-04 17:19:11 -0700

    added random enemy gen. Added main menu. added player pages. das all folks

---
+++ The_Game/Assets/playerPageHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerPageHandler : MonoBehaviour
+{
+
+    public GameObject player;
+    void Start()
+    {
+        Instantiate(player);
+    }
+}
